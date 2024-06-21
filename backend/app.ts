d7b82d7a89987e1f47401053776b4840335a9e38
powder <powder@mail.com>
2024-06-21 16:11:16 +0700

    final models and add tailwindcss

--- backend/app.ts
+++ backend/app.ts
@@ -1,16 +1,14 @@
-import express from "express";
-require("dotenv").config();
+import express from 'express';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
-
-
-
+  console.log(`Server is running on port ${port}`);
+});
